02cab89c2fbb9c932cc98baacf78633b20682ca4
본ìjinyun_J < ppp6289@gmail.com>
2023-09-18 13:35:08 +0900

    커밋할게?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,22 @@
 package org.example;
 
-// Shift을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
-// Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
+
 public class Main {
     public static void main(String[] args) {
-        // 캐럿을 강조 표시된 텍스트에 놓고 Ctrl+1을(를) 누르면
-        // IntelliJ IDEA의 수정 제안을 볼 수 있습니다.
-        System.out.printf("Hello and welcome!");
-        int a = 1;
-        String b = "수정 하였습니다.";
-        // Alt+Shift+X을(를) 누르거나 여백의 녹색 화살표 버튼을 클릭하여 코드를 실행합니다.
-        for (int i = 1; i <= 5; i++) {
 
-            // Alt+Shift+D을(를) 눌러 코드 디버그를 시작합니다. 중단점을 하나 설정해 드렸습니다.
-            // 중단점을 더 추가하려면 언제든지 Ctrl+Shift+B을(를) 누르세요.
-            System.out.println("i = " + i);
-        }
+        int num1 = 10 ;
+        int num2 = 5 ;
+        int temp = 0;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("변수를 스왑합니다.");
+         temp = num1 ; // temp에 num1(10) 대입
+         num1 = num2 ; // num1에 5 대입
+         num2 = temp ;
+
+        System.out.println(num1);
+        System.out.println((num2));
+
     }
+
 }
